da6deeb6f5040c134ba2d1357a57fe8e5219c227
Mason_Mei <listener_mei@163.com>
2014-10-30 12:43:08 +0800

    update

---
+++ ui_control_classes.cpp
@@ -0,0 +1,47 @@
+/*********************************************
+*     MadeBy : MeiZhaorui(Mason)
+*     E-Mail : listener_mei@163.com
+*      Phone : (0)131-5898-7498
+*       Date : 2014/10/29
+*       host : Ubuntu x86_64 3.13.0-37
+ *********************************************/
+#include "ui_control_classes.h"
+
+base_set_weidget::base_set_weidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::base_set_weidget)
+{
+    ui->setupUi(this);
+}
+
+base_set_weidget::~base_set_weidget()
+{
+    delete ui;
+}
+
+/*******************************************************************************************/
+
+play_set_widget::play_set_widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::play_set_widget)
+{
+    ui->setupUi(this);
+}
+play_set_widget::~play_set_widget()
+{
+    delete ui;
+}
+/*******************************************************************************************/
+
+about_widget::about_widget( QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::about_widget)
+{
+    ui->setupUi(this);
+}
+
+about_widget::~about_widget()
+{
+    delete ui;
+}
+
---
+++ ui_control_classes.h
@@ -0,0 +1,57 @@
+/*********************************************
+*     MadeBy : MeiZhaorui(Mason)
+*     E-Mail : listener_mei@163.com
+*      Phone : (0)131-5898-7498
+*       Date : 2014/10/29
+*       host : Ubuntu x86_64 3.13.0-37
+ *********************************************/
+
+#ifndef UI_CONTROL_CLASSES_H
+#define UI_CONTROL_CLASSES_H
+#include <QWidget>
+#include "shadow_widget.h"
+#include "ui_base_set_weidget.h"
+#include "ui_play_set_widget.h"
+#include "ui_about_widget.h"
+#include "ui_messagebox.h"
+
+/**********************************************/
+class base_set_weidget : public QWidget
+{
+    Q_OBJECT
+    friend class down_widget_;
+public:
+    explicit base_set_weidget(QWidget *parent = 0);
+    ~base_set_weidget();
+private:
+    Ui::base_set_weidget *ui;
+};
+
+/**********************************************/
+
+class play_set_widget : public QWidget
+{
+    Q_OBJECT
+    friend class down_widget_;
+public:
+    explicit play_set_widget(QWidget *parent = 0);
+    ~play_set_widget();
+private:
+    Ui::play_set_widget *ui;
+};
+
+/**********************************************/
+
+class about_widget : public QWidget
+{
+    Q_OBJECT
+    friend class down_widget_;
+public:
+    explicit about_widget(QWidget *parent = 0);
+    ~about_widget();
+private:
+    Ui::about_widget *ui;
+};
+
+
+#endif // UI_CONTROL_CLASSES_H
